6009f8447e794a6cf44bc0095feb6e8a4bdb5012
poojachandurkar1-maker <poojachandurkar1@gmail.com>
2026-07-20 18:24:07 +0530

    Initial Spring Boot setup with PostgreSQL connection

---
+++ src/main/java/com/infotact/atmosync/AtmosyncApplication.java
@@ -0,0 +1,13 @@
+package com.infotact.atmosync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmosyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtmosyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infotact/atmosync/AtmosyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotact.atmosync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmosyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
