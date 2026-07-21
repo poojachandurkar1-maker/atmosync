e0a1bf930d01a373cab8a1ff9938b8f71b5b9ded
poojachandurkar1-maker <poojachandurkar1@gmail.com>
2026-07-21 17:27:06 +0530

    Added Python script for bulk sensor data generation

--- python/generate_sensor_data.py
+++ python/generate_sensor_data.py
@@ -1,23 +1,41 @@
 import pandas as pd
-from faker import Faker
 import random
+from faker import Faker
 from sqlalchemy import create_engine
+from datetime import datetime, timedelta
 
 fake = Faker()
 
 engine = create_engine(
-    "postgresql+psycopg2://postgres:postgres@localhost:5432/atmosync"
+    "postgresql://postgres:postgres@localhost:5432/atmosync"
 )
 
 records = []
 
+locations = [
+    "Nagpur",
+    "Mumbai",
+    "Delhi",
+    "Pune",
+    "Hyderabad",
+    "Bangalore"
+]
+
 for i in range(10000):
+
     records.append({
-        "container_id": f"CONT{random.randint(100,999)}",
-        "temperature": round(random.uniform(15,40),2),
-        "humidity": round(random.uniform(40,95),2),
-        "location": fake.city(),
-        "timestamp": fake.date_time_this_year()
+
+        "container_id": f"CONT{random.randint(1000,9999)}",
+
+        "temperature": round(random.uniform(5,45),2),
+
+        "humidity": round(random.uniform(20,95),2),
+
+        "location": random.choice(locations),
+
+        "timestamp":
+        datetime.now()-timedelta(hours=random.randint(0,720))
+
     })
 
 df = pd.DataFrame(records)
@@ -29,4 +47,4 @@ df.to_sql(
     index=False
 )
 
-print("10000 sensor records inserted successfully!")
+print("10000 Records Inserted Successfully")
